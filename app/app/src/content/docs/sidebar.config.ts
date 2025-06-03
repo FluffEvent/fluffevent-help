a37b7e56e4745be4fab7060ce6f697b9724ae975
Matiboux <matiboux@gmail.com>
2025-06-03 02:27:50 +0200

    Add root sidebar config

---
+++ app/app/src/content/docs/sidebar.config.ts
@@ -0,0 +1,22 @@
+import type { StarlightUserConfig } from '@astrojs/starlight/types'
+
+const sidebar: NonNullable<StarlightUserConfig['sidebar']> = [
+	{
+		label: '🐾 fluffevent.fr',
+		link: 'https://fluffevent.fr',
+		attrs: {
+			target: '_blank',
+		},
+	},
+	{
+		label: 'Home',
+		slug: 'index',
+	},
+	{
+		label: 'Fluff Event 2023',
+		// FIXME: Add an index page for this section
+		slug: 'fluff-event-2023/faq',
+	}
+]
+
+export default sidebar
