0d9bb17538869fab2f7a8973186e1c0608937627
Mati <matiboux@gmail.com>
2026-03-04 13:09:13 +0100

    Add 2026 event pages (#26)

---
+++ app/src/content/docs/fluff-event-2026/sidebar.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('@astrojs/starlight/types').StarlightUserConfig} */
+const sidebar = [
+	{
+		label: '⬅️ Retour à l’accueil',
+		translations: {
+			en: '⬅️ Back to home page',
+		},
+		slug: '::back',
+	},
+	{
+		label: 'Fluff Event 2026',
+		slug: 'fluff-event-2026',
+	},
+	{
+		label: 'Association soutenue',
+		translations: {
+			en: 'Supported charity',
+		},
+		slug: 'fluff-event-2026/charity',
+	},
+]
+
+export default sidebar
