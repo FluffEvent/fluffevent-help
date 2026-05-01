4689fcc3a766935d61b4a019e5b3ecefe1190cb1
Mati <matiboux@gmail.com>
2026-05-01 16:00:12 +0200

    Update Fluff Event links (#29)

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
 				},
 			},
 			social: [
-				{ icon: 'twitter', label: 'Twitter', href: 'https://twitter.com/FluffEvent' },
+				{ icon: 'twitter', label: 'Twitter', href: 'https://x.com/FluffEvent' },
 				{ icon: 'telegram', label: 'Telegram', href: 'https://t.me/FluffEventNews' },
 				{ icon: 'discord', label: 'Discord', href: 'https://discord.com/invite/rAesnJHuPe' },
 				{ icon: 'github', label: 'GitHub', href: 'https://github.com/FluffEvent/fluffevent-help' },
